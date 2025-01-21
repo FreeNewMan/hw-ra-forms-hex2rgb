d1a536bb5911ca65bb0b57ae7bc835108f728437
FreeNewMan <lutovp@yandex.ru>
2025-01-21 19:32:09 +0500

    bug fix

--- src/models/index.ts
+++
@@ -1,9 +0,0 @@
-export interface ICard {
-    listing_id: number;
-    url: string;
-    MainImage: string;
-    title: string;
-    currency_code: string; 
-    price: string;
-    quantity: string; 
-  }
